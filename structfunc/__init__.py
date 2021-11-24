831457cbc59435e2e2049be6e1432712f2b0a461
jdhenshaw <jonathan.d.henshaw@gmail.com>
2021-11-24 10:46:30 +0100

    package config

--- structfunc/__init__.py
+++ structfunc/__init__.py
@@ -7,7 +7,8 @@ from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
 __all__ = []
-from .example_mod import *   # noqa
+from .structfunc1d import StructFunc1D
+from .structfunc2d import StructFunc2D  # noqa
 # Then you can be explicit to control what ends up in the namespace,
 __all__ += ['do_primes']   # noqa
 # or you can keep everything from the subpackage with the following instead
